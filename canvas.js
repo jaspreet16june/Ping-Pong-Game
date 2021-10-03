e9916c049c4c3bae0c514493f4355b94d72818ef
jaspreet kaur <jaspreetkaur16june@gmail.com>
2021-10-03 16:33:35 +0530

    Added animation

--- canvas.js
+++ canvas.js
@@ -11,29 +11,68 @@ canvas.width = width;
 canvas.height = height
 
 
-context.fillStyle = "black";
-context.fillRect(0,0,width,height)
+const circle = {
+    x: 200,
+    y:200,
+    dx : 3,
+    dy:4,
+    size:9,
+}
+function score(){
+    context.font = "20px Birthstone";
+    context.fillText("10", 10 ,height/2-20 )
 
-context.fillStyle = "white";
-context.fillRect(130,10,slideWidth,slideHeight)
+    context.font = "20px Birthstone";
+    context.fillText("30",10,height/2+40)
+}
 
-context.fillStyle = "white";
-context.fillRect(130,490,slideWidth,slideHeight)
+function drawRect(){
+    context.fillStyle = "black";
+    context.fillRect(0,0,width,height)
 
-context.beginPath();
-context.moveTo(0,height/2);
-context.lineTo(width,height/2);
-context.setLineDash([4]);
-context.strokeStyle = "white"
-context.stroke();
+    context.fillStyle = "white";
+    context.fillRect(130,10,slideWidth,slideHeight)
 
-context.beginPath();
-context.arc(width/2,height/2,8,0,Math.PI*2,false);
-context.fillStyle="white";
-context.fill();
+    context.fillStyle = "white";
+    context.fillRect(130,490,slideWidth,slideHeight)
+}
 
-context.font = "20px Birthstone";
-context.fillText("10", 10 ,height/2-20 )
 
-context.font = "20px Birthstone";
-context.fillText("30",10,height/2+40)
+function drawSlits(){
+    context.beginPath();
+    context.moveTo(0,height/2);
+    context.lineTo(width,height/2);
+    context.setLineDash([4]);
+    context.strokeStyle = "white"   
+    context.stroke();
+}
+function drawCircle(){
+    context.beginPath();    
+    context.arc(circle.x,circle.y,circle.size,0,Math.PI*2,false);
+    context.fillStyle="white";
+    context.fill();
+}
+
+function update(){
+    context.clearRect(0,0,canvas.width,canvas.height);
+    drawRect()
+    drawSlits()
+    score();
+    drawCircle();
+
+    circle.x += circle.dx;
+    circle.y += circle.dy;
+
+    if(circle.x + circle.size > canvas.width ||circle.x - circle.size < 0 ){
+        circle.dx *= -1;
+    }
+
+    if(circle.y + circle.size > canvas.height ||circle.y - circle.size < 0 ){
+        circle.dy *= -1;
+    }
+
+    requestAnimationFrame(update);
+
+}
+
+update();
